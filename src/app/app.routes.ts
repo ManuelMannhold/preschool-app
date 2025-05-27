dc4744782a910d7ad31d215fa75997ea7d269f56
ManuelMannhold <M.Mannhold@gmx.de>
2025-05-27 18:52:25 +0200

    add function for random number of apples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MaincomponentComponent } from './maincomponent/maincomponent.component';
+import { LevelComponent } from './gameLevel/level/level.component';
 
 export const routes: Routes = [
-    {path: '', component: MaincomponentComponent}
+    {path: '', component: MaincomponentComponent},
+    {path: 'count-game', component: LevelComponent}
 ];
---
+++ src/app/gameLevel/level/level.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level',
+  standalone: true,
+  imports: [],
+  templateUrl: './level.component.html',
+  styleUrl: './level.component.scss'
+})
+export class LevelComponent {
+  appleImage: string = '/assets/img/apple_';
+  apples: string[] = [];
+
+  public ngOnInit(): void {
+    this.constructApples();
+  }
+
+  constructApples() {
+    const appleCount = Math.floor(Math.random() * 6) + 1;
+    this.apples = [];
+
+    for (let i = 0; i < appleCount; i++) {
+      const randomAppleNumber = Math.floor(Math.random() * 6);
+      this.apples.push(`${this.appleImage}${randomAppleNumber}.png`);
+    }
+  }
+}
